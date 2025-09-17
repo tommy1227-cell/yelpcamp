c4f08b78d8d97921ab2e21bd500e3c458b884476
tommy1227 <tommydan1227@gmail.com>
2025-09-17 15:15:22 +0900

    my first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const ExpressError = require('./utils/ExpressError.js')
+const Campground = require('./models/campground.js');
+const catchAsync = require('./utils/catchAsync');
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
+const Joi =require('joi');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
+    .then(() => {
+        console.log('接続しています')
+    })
+    .catch(err => {
+        console.log('接続できていません')
+    });
+
+app.engine('ejs', ejsMate);
+app.set('views', path.join(__dirname, 'views')); //getでテンプレートはviewsから探してください
+app.set('view engine', 'ejs'); //ejsを使って書く宣言
+
+//ミドルウェア（ログイン認証とかにも使える）
+app.use(express.urlencoded({ extended: true })); //データをとってくるときに必要
+app.use(methodOverride('_method')); //get post以外のformのHTTPメソッドの時に必要
+app.use((req, res, next) => {
+    console.log('ミドルウェアを通過');
+    next(); //ここで止めないためのもの
+});//ミドルウェアの通過の確認
+app.use(morgan('tiny')); //ログ管理
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get('/campgrounds', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}));
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs',)
+})
+
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+}));
+
+app.get('/practice', (req, res) => {
+    res.send('接続確認');
+});
+
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+}));
+
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('不正なキャンプデータです', 400);
+    const campgroundSchema =Joi.object({
+        campground: Joi.object({
+            title:Joi.string().required(),
+            price:Joi.number().required().min(0),
+        }).required()
+    });
+
+    const result=campgroundSchema.validate(req.body);
+    console.log(result);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+}));
+
+app.use((req, res, next) => {
+    next(new ExpressError('ページが見つかりません', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500,} = err;
+    if (!err.message){
+        err.message='問題が起きました'
+    }
+    res.status(statusCode).render('error',{err})
+});
+
+app.listen(3000, () => {
+    console.log('接続中');
+});
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+
+const mongoose = require('mongoose');
+const Campground= require('../models/campground.js');
+const cities = require('./cities.js')
+const {descriptors,places} = require('./seedHelpers.js');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
+  .then(() => {
+    console.log('接続しています')
+  })
+  .catch (err => {
+    console.log('接続できていません')
+  });
+
+const sample = (array) =>array[Math.floor(Math.random()*array.length)];
+
+const seedDB = async () =>{
+    await Campground.deleteMany({});
+    for (let i=0; i < 50; i++){
+        const randomCityIndex =Math.floor(Math.random()*cities.length);
+        const price = Math.floor(Math.random()*2000+1000);
+        const camp = new Campground({
+            location:`${cities[randomCityIndex].prefecture}`,
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description:'ダミーテキスト',
+            price:price
+        });
+        await camp.save()
+        
+    }
+  }
+
+seedDB().then(()=> {
+    mongoose.connection.close();
+});
